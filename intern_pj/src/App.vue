9a5c8777803c606c27fc998531b2688289d4e164
Jiraphat <151009008+csjiraphat@users.noreply.github.com>
2025-11-17 22:14:40 +0700

    add main theme&fix syle login

--- intern_pj/src/App.vue
+++ intern_pj/src/App.vue
@@ -1,13 +1,10 @@
 <template>
   <Navbar/>
   <RouterView />
-  <login-page/>
-  <register-page/>
 </template>
 
 <script setup lang="ts">
 import Navbar from '@/components/Navbar.vue'
 import { RouterView } from 'vue-router'
-import RegisterPage from './pages/RegisterPage.vue';
 </script>
 <style scoped></style>
--- intern_pj/src/main.ts
+++ intern_pj/src/main.ts
@@ -5,6 +5,7 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import "./styles/main.css"
 import './assets/main.css'
 import '@mdi/font/css/materialdesignicons.css';
 const app = createApp(App)
