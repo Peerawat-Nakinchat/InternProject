abea8951d491b211bfa40af7c1274b7239a2bc7c
Peerawat-Nakinchat <650112230015@bru.ac.th>
2025-11-18 10:04:21 +0700

    update Navbar

---
+++ intern_pj/src/layouts/MainLayout.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="flex flex-col min-h-screen">
+
+    <!-- Navbar -->
+    <Navbar />
+
+    <!-- Sidebar + Content -->
+    <div class="flex flex-1">
+
+      <!-- Sidebar -->
+      <Sidebar />
+
+      <!-- Main Content (ไม่ต่อจาก Navbar) -->
+      <main class="flex-1 bg-gray-100 p-6 overflow-auto">
+        <router-view />
+      </main>
+
+    </div>
+  </div>
+</template>
+
+<script setup>
+import Navbar from '@/components/Navbar.vue'
+import Sidebar from '@/components/Sidebar.vue'
+</script>
