9cfc8f1bac0d9d7d18f2ebfa59a56981f1ccfebc
Jiraphat <151009008+csjiraphat@users.noreply.github.com>
2025-11-20 10:45:48 +0700

    fatch register

--- intern_pj/src/components/base/BaseInput.vue
+++ intern_pj/src/components/base/BaseInput.vue
@@ -10,6 +10,14 @@
       v-model="innerValue"
       class="w-full rounded-md border px-3 py-2 text-sm border-neutral-300 bg-white focus:outline-none focus:ring-2 focus:ring-primary-200 focus:border-primary-500 placeholder:text-slate-400 disabled:bg-neutral-100 disabled:text-neutral-500 disabled:cursor-not-allowed disabled:border-neutral-300"
     />
+
+    <div
+        v-if="$slots.append"
+        class="absolute inset-y-0 right-3 flex items-center cursor-pointer select-none"
+      >
+        <slot name="append" />
+      </div>
+
     <p v-if="error" class="text-xs text-red-500 absolute pointer-events-none mt-0.5">
       {{ error }}
     </p>
