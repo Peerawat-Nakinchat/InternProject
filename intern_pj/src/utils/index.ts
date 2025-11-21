fe75459382f5d00b3cc83bf9258698e0bba4b74e
Jiraphat <151009008+csjiraphat@users.noreply.github.com>
2025-11-21 14:22:11 +0700

    create iso test page devlop ag-grid

--- intern_pj/src/utils/index.ts
+++ intern_pj/src/utils/index.ts
@@ -1,7 +1,19 @@
 import axios from 'axios';
 import { defineStore } from 'pinia'
+import {
+  AllCommunityModule,
+  ModuleRegistry,
+  type ColDef,
+  type GridApi,
+  type GridReadyEvent,
+} from 'ag-grid-community'
 
 export {
   axios,
-  defineStore
+  defineStore,
+  AllCommunityModule,
+  ModuleRegistry,
+  type ColDef,
+  type GridApi,
+  type GridReadyEvent,
 }
